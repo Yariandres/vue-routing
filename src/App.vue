73137464b7996ea626af4d410a1b3ecdec0692d4
Yari Andres <yari.andres@gmail.com>
2020-12-25 00:00:07 +0100

    editing app to original state

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,13 @@ export default {
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
         { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
       ],
+      users: [
+        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
+        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
+        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
+        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
+        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
+      ],
     };
   },
   provide() {
